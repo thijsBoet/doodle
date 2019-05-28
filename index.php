85bb1b5201f72d1bd03558ad4177e828e34a6282
Matthijs Boet <m.boet@chello.nl>
2019-05-28 23:00:57 +0200

    migrated from cloud9 Repo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Doodle</title>
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+    </head>
+    <body>
+        <div class="wrapper indexPage">
+            <div class="mainSection">
+                
+                <div class="logoContainer">
+                    <img src="assets/images/doodleLogo.png" title="Doodle Logo" alt="Doodle logo"></img>
+                </div>    
+    
+                <div class="searchContainer">
+                    
+                    <form action="search.php" method="GET">
+                        
+                            <input class="searchBox" type="text" name="term"/>
+                            <input class="searchButton" type="submit" value="Search"/>
+                            
+                        </div>
+                    </form>
+                    
+            </div>
+        
+        </div>
+    </body>
+</html>
